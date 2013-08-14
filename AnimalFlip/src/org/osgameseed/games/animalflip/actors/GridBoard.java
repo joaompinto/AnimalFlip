b1550a56af9c11bf7d047bbb2880da2417476d34
João Pinto <lamego.pinto@gmail.com>
2013-08-14 11:42:22 +0100

    Added label when counting aimals

--- AnimalFlip/src/org/osgameseed/games/animalflip/actors/GridBoard.java
+++ AnimalFlip/src/org/osgameseed/games/animalflip/actors/GridBoard.java
@@ -1,14 +1,21 @@
 package org.osgameseed.games.animalflip.actors;
 
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Action;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.utils.Array;
+
 
 import org.osgameseed.games.animalflip.Assets;
 
@@ -16,25 +23,82 @@ import java.util.Random;
 
 public class GridBoard extends Group {
 
-    public Array<Actor> recreate; // list of images that need to be recreated
+    BitmapFont font;                // For the touch count label
+    Label.LabelStyle font_style;
+    Random rand;
+
+    int touched_counter; // count of cards that have been touched
+    int last_card_face; // last card face index
+
+    int placed_faces_count[]; // list of counts per card face
+    Array<Actor> flipped_cards; // list of flipped cards
+    Array<Actor> placed_numbers; // list of placed numbers;
 
     public GridBoard() {
 
-        Random rand = new Random();
-        recreate = new Array<Actor>();
 
-        for(int i=0; i < 3*5; i++) {
+        rand = new Random();
+
+        font = new BitmapFont(Gdx.files.internal("data/fonts/numbers.fnt"),
+                Gdx.files.internal("data/fonts/numbers.png"), false);
+        font_style = new Label.LabelStyle(font, Color.WHITE);
+
+        touched_counter = 0;
+        last_card_face = -1;
+
+        flipped_cards = new Array<Actor>();
+        placed_numbers = new Array<Actor>();
+        placed_faces_count = new int[Assets.animal_textures.size];
+
+        int rows;
+        if(Gdx.app.getType() == Application.ApplicationType.Desktop)
+            rows = 4;
+        else
+            rows = 3;
+        int columns = 5;
+
+        // Fill the board with random cards
+        for(int i=0; i < rows*columns; i++) {
             int rand_card_i = rand.nextInt(Assets.animal_textures.size);
-            //System.out.println("Added "+rand_card_i+" "+card.getWidth());
-            Image card = new Image(Assets.animal_textures.get(rand_card_i));
-            float left_margin = 20;
-            float top_margin = 200;
-            float x_pos = left_margin+(i % 5)*155;
-            float y_pos = (i / 5)*150;
-            card.setPosition(x_pos, y_pos);
-            card.setName(Integer.toString(i));
-            addActor(card);
+            addCard(rand_card_i, i);
         }
+    }
+
+    private Actor addCard(int card_face, float x, float y) {
+
+        // Create the card object
+        Image card = new Image(Assets.animal_textures.get(card_face));
+        card.setPosition(x, y);
+        card.setName(Integer.toString(card_face));
+
+        // Set to invisible and not touchable on start
+        Color color = card.getColor();
+        card.setColor(color.r, color.g, color.b, 0f);
+        card.setTouchable(Touchable.disabled);
+
+        addActor(card);
+
+        placed_faces_count[card_face]++; // Increment the faces count for the new card
+
+        //
+        card.addAction(
+                Actions.sequence(
+                        Actions.fadeIn(0.5f),
+                        new on_flip_in()
+                )
+        );
+
+        return card;
+    }
+
+    private Actor addCard(int card_face, int board_pos) {
+
+        // Create the card object
+        Image card = new Image(Assets.animal_textures.get(card_face));
+        float left_margin = 20;
+        float x = left_margin+(board_pos % 5)*155;
+        float y = (board_pos / 5)*150;
+        return addCard(card_face, x, y);
     }
 
     @Override
@@ -42,22 +106,87 @@ public class GridBoard extends Group {
         super.draw(batch, parentAlpha);
     }
 
-    public class regenerate extends Action {
+    public class InputHandler extends InputListener {
+
+
+        public InputHandler() {
+        }
+
+        public boolean touchDown(InputEvent event, float x, float y,
+                                 int pointer, int button) {
+            Actor hit_card = hit(x, y, true);
+            if(hit_card != null) {
+                int hit_card_face = Integer.parseInt(hit_card.getName());
+
+                if(last_card_face != -1 && hit_card_face != last_card_face) // Not with the same face
+                    return true;
+
+                last_card_face =   hit_card_face;
+                flipped_cards.add(hit_card);
+
+                Label countLabel = new Label(Integer.toString(flipped_cards.size), font_style);
+                countLabel.setTouchable(Touchable.disabled);
+
+                float pos_x = hit_card.getX() + hit_card.getWidth()/2-30;
+                float pos_y = hit_card.getY() + hit_card.getHeight()/2-30;
+                countLabel.setPosition(pos_x, pos_y);
+
+                addActor(countLabel);
+                placed_numbers.add(countLabel);
+
+                hit_card.setTouchable(Touchable.disabled);
+                hit_card.addAction(
+                        Actions.sequence(
+                                Actions.fadeOut(0.5f),
+                                new on_flip_out()
+                        )
+                );
+            }
+            return true;
+        }
+
+    }
+
+    public class on_flip_in extends Action {
 
         @Override
         public boolean act(float delta) {
-            System.out.println("regenerating");
-            Actor to_delete = recreate.removeIndex(0);
-            float x_pos = to_delete.getX();
-            float y_pos = to_delete.getY();
-            String old_name = to_delete.getName();
-            to_delete.remove();
-            Random rand = new Random();
-            int rand_card_i = rand.nextInt(Assets.animal_textures.size);
-            Image card = new Image(Assets.animal_textures.get(rand_card_i));
-            card.setPosition(x_pos, y_pos);
-            card.setName(old_name);
-            addActor(card);
+
+            Actor flipped_card = getActor();
+            flipped_card.setTouchable(Touchable.enabled);
+            return true;
+        }
+
+    }
+
+    public class on_flip_out extends Action {
+
+        @Override
+        public boolean act(float delta) {
+
+            Actor flipped_card = getActor();
+
+            int flipped_card_face = Integer.parseInt(flipped_card.getName());
+
+            // Check if the last card was touched
+            if(flipped_cards.size == placed_faces_count[flipped_card_face]) {
+                for(Actor old_card: flipped_cards) {
+                    // Remove the old card
+                    float old_x = old_card.getX();
+                    float old_y = old_card.getY();
+                    int old_card_face = Integer.parseInt(old_card.getName());
+                    old_card.remove();
+                    placed_faces_count[old_card_face]--;
+                    int rand_card_i = rand.nextInt(Assets.animal_textures.size);
+                    addCard(rand_card_i, old_x, old_y);
+                    last_card_face = -1;
+                }
+                flipped_cards.clear();
+                for(Actor number: placed_numbers) {
+                    number.remove();
+                }
+                placed_numbers.clear();
+            }
             return true;
         }
 
--- AnimalFlip/src/org/osgameseed/games/animalflip/screens/PlayScreen.java
+++ AnimalFlip/src/org/osgameseed/games/animalflip/screens/PlayScreen.java
@@ -7,7 +7,6 @@ import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
 import org.osgameseed.games.animalflip.AnimalFlipGame;
-import org.osgameseed.games.animalflip.InputHandler;
 import org.osgameseed.games.animalflip.actors.GridBoard;
 
 public class PlayScreen implements Screen {
@@ -25,12 +24,10 @@ public class PlayScreen implements Screen {
 
         board = new GridBoard();
         stage = new Stage();
-        InputHandler inputHandler = new InputHandler(board);
 
         Gdx.input.setInputProcessor(stage);
 
-
-        stage.addListener(inputHandler);
+        stage.addListener(board.new InputHandler());
         stage.addActor(board);
     }
 
@@ -53,34 +50,6 @@ public class PlayScreen implements Screen {
         stage.act(delta);
         stage.draw();
 
-        /*
-        batch.setProjectionMatrix(camera.combined);
-        batch.begin();
-
-        Iterator<Integer> iter = cards_grid.iterator();
-        int count = 0;
-        while(iter.hasNext()) {
-            int card_i = iter.next();
-            float left_margin = 20;
-            float top_margin = (h/2)-200;
-            float x_pos = left_margin+(-w/2)+(count % 5)*155;
-            float y_pos = top_margin-(count / 5)*150;
-            //Sprite sprite = animals_sprites.get(card_i);
-            //sprite.setScale(1);
-            //sprite.setPosition(x_pos, y_pos);
-            //sprite.draw(batch);
-            ++count;
-        }
-        batch.end();
-
-
-        // process user input
-        if(Gdx.input.isTouched()) {
-            Vector3 touchPos = new Vector3();
-            touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-            camera.unproject(touchPos);
-            System.out.print(touchPos.x);
-        }*/
     }
 
     @Override
